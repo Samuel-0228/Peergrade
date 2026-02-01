c2077c1747456c79ce8b32c44291aff9efa149ba
samuelytt <samuaelyeshambel@gmail.com>
2026-02-01 12:49:32 +0300

    update 12.3

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -6,8 +6,8 @@ import { createClient } from '@supabase/supabase-js';
  * Environment variables are preferred for deployment, with defaults for development.
  */
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY ;
+const supabaseUrl = process.env.SUPABASE_URL || 'https://zaztjiljjqvpmxjcdten.supabase.co';
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphenRqaWxqanF2cG14amNkdGVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4NjI0NjcsImV4cCI6MjA3ODQzODQ2N30.kmXhS5DOr-k3Tx_FOGLr7IXa-Df8QtTNaxpzBMU-0JA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
